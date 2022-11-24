19142eb008ad468d1f922b29a64c333a9125336f
jiii-9 <succeed.g9@gmail.com>
2022-11-24 16:27:46 +0900

    add project component folders and files

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = props => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        {/* class이름에 -가 있으면 점표기법을 사용할 수 없다. */}
+        <img src={mealsImage} alt="A table full of deliciout food!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
